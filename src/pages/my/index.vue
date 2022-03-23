9639a313c0b6829703920be72228e09e6026c5dc
tanglinxuework@163.com <tanglinxuework@163.com>
2022-03-23 22:52:34 +0800

    开发

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,52 +1,57 @@
 <template>
-	<view class='content'>
+	<view class='main column-center'>
 		<view class='top'>
-			<view>
-				<view>
-					<view><image src='https://dongjiaodaojia.oss-cn-chengdu.aliyuncs.com//uploads/20211230/7f5c22d055822ee8fdabca116f354cbf.png'></image></view>
-					<view>
-						<view>张一山</view>
-						<view>普通会员</view>
+			<view class='row-between info'>
+				<view class='left row-start'>
+					<view class='imgbox'><image src='https://dongjiaodaojia.oss-cn-chengdu.aliyuncs.com//uploads/20211230/7f5c22d055822ee8fdabca116f354cbf.png'></image></view>
+					<view class='column-center-a'>
+						<view class='name'>张一山</view>
+						<view class='vip'>普通会员</view>
 					</view>
 				</view>
-				<image src='../../static/images/my/ic-setup.png'></image>
+				<image src='../../static/images/my/ic-setup.png' class='icon'></image>
 			</view>
-			<image></image>
+			<!-- <view>
+				<view></view>
+				<view>立即充值</view>
+			</view> -->
 		</view>
 		<view class='row-between collection'>
-			<view>
-				<text>我的收藏</text>
-				<text>22</text>
+			<view class='item column-center'>
+				<text class='txt1'>我的收藏</text>
+				<text class='txt2'>22</text>
 			</view>
-			<view>
-				<text>优惠券</text>
-				<text>6</text>
+			<view class='item column-center'>
+				<text class='txt1'>优惠券</text>
+				<text class='txt2'>6</text>
 			</view>
 		</view>
 		<view class='fun'>
-			<view>其他功能</view>
+			<view class='title'>其他功能</view>
 			<view class='row-start'>
-				<view>
+				<view class='item column-center'>
 					<image src='../../static/images/my/pic-kefu.png'></image>
 					<text>联系客服</text>
 				</view>
-				<view>
+				<view class='item column-center'>
 					<image src='../../static/images/my/pic-diz.png'></image>
 					<text>地址管理</text>
 				</view>
-				<view>
+				<view class='item column-center'>
 					<image src='../../static/images/my/pic-zhaomu.png'></image>
 					<text>技师招募</text>
 				</view>
 			</view>
 		</view>
-		<image src='../../static/images/my/pic-share.png'></image>
+		<!-- <image src='../../static/images/my/pic-share.png' class='share-img'></image> -->
 		<view class='recommend'>
-			<view class='row-center'>
+			<view class='row-center title'>
 				<image src='../../static/images/my/pic-gift.png'></image>
-				<text></text>
+				<text>为您推荐</text>
 			</view>
 			<view class="list column-center">
+				<technician></technician>
+				<technician></technician>
 				<technician></technician>
 			</view>
 		</view>
@@ -60,4 +65,114 @@
 
 <style lang="scss" scoped>
 @import '../../static/scss/index.scss';
+.main{
+	padding-bottom: 110rpx;
+	.top{
+		width:100%;
+		height:480rpx;
+		background:url(../../static/images/my/pic-bc.png);
+		background-size: 100% 100%;
+		padding:120rpx 30rpx 0;
+		margin-bottom: 20rpx;
+		.info{
+			.left{	
+				.imgbox{
+					width:150rpx;
+					height:150rpx;
+					border:6rpx solid #fff;
+					border-radius: 100%;
+					margin-right: 20rpx;
+					image{
+						width:100%;
+						height:100%;
+						border-radius: 100%;
+					}
+				}
+				.name{
+						font-size:40rpx;
+						font-weight: bold;
+						color:#fff;
+						margin-bottom: 15rpx;
+				}
+				.vip{
+					
+						width: 140rpx;
+						height: 44rpx;
+						line-height: 44rpx;
+						background: rgba(0, 0, 0, 0.6);
+						border-radius: 22rpx;
+						text-align: center;
+						color: #fff;
+						font-size: 26rpx;
+					
+				}
+			}
+			.icon{
+				width:56rpx;
+				height:56rpx
+			}
+		}
+	}
+	
+	.collection{
+		width:710rpx;
+		background: #fff;
+		padding:30rpx;
+		box-shadow: 0px 0px 8rpx 0px rgba(218, 218, 218, 0.5);
+		border-radius: 12rpx;
+		margin-bottom: 20rpx;
+		.item{
+			width:312rpx;
+			height:140rpx;
+			background:url(../../static/images/my/pic-background.png);
+			background-size: 100% 100%;
+			color:#fff;
+			.txt2{
+				font-size: 56rpx;
+				font-weight: bold;
+			}
+		}
+	}
+	.fun{
+		width:710rpx;
+		background: #fff;
+		padding:40rpx 30rpx;
+		box-shadow: 0px 0px 8rpx 0px rgba(218, 218, 218, 0.5);
+		border-radius: 12rpx;
+		.title{
+			font-size: 32rpx;
+			margin-bottom: 30rpx;
+		}
+		.item{
+			width:100rpx;
+			margin-right: 90rpx;
+			image{
+				width:90rpx;
+				height:90rpx;
+				
+				margin-bottom: 15rpx;
+			}
+			text{
+				font-size: 26rpx;
+			}
+		}
+	}
+	.recommend{
+		.title{
+			padding-top:40rpx;
+			margin-bottom: 20rpx;
+			image{
+				width: 48rpx;
+				height:48rpx;
+				margin-right: 12rpx;
+			}
+			text{
+				font-size:40rpx
+			}
+		}
+		
+	}
+
+}
+
 </style>
